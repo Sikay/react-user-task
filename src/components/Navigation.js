276a3653ad34996792e1f6c2ea4c88158ccaa926
Sikay <pedrohdezmora@gmail.com>
2020-03-09 20:51:52 +0100

    Cambio a class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./Navigation.css";
 
-function Navigation() {
-  return (
-    <nav className="Menu">
-      <img src={logo} className="Menu-logo" alt="logo" />
-      <div className="Menu-link">
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Task
-        </a>
-      </div>
-      <div className="Menu-react">
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </div>
-    </nav>
-  );
+class Navigation extends Component {
+  render() {
+    return (
+      <nav className="Menu">
+        <img src={logo} className="Menu-logo" alt="logo" />
+        <div className="Menu-link">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Task
+          </a>
+        </div>
+        <div className="Menu-react">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;
